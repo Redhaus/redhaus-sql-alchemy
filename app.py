4b903eb4634acda5b6d33a8dd310e04df406ae63
Benjamin Read <redbook@Benjamins-MacBook-Pro-3.local>
2019-12-10 20:26:15 -0800

    updated procfile

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
+# import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+# import config file
+app.config.from_pyfile('config.cfg')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# create db obj with SQLalchemy and pass it app
+db = SQLAlchemy(app)
+
+# create first table with sqlAlchemy
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
 
 if __name__ == '__main__':
